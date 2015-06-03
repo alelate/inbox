3a5afeb16ff0f5a6444af66cfb9159bd3fcf730f
Aleksejs <alelate69@gmail.com>
2015-06-04 02:01:13 +0300

    Updates script.js 0.3a

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 $(document).ready(function () {
-
-    $(".name").hide();
-
-    $("#search").keyup(function () {
-
-        $(".name").hide();
-
-        var val = $(this).val().toLowerCase();
-
-        $(".name").each(function () {
-
-            var text = $(this).text().toLowerCase();
-
-
-            if(val.length !== 0) { 
-
-                if (text.indexOf(val) !== -1 && val.charAt(val.length - 1) === text.charAt(val.length - 1)) {
-
-                    $(this).show();
-            }  }
-        });
-    });
-});
+  
+      $(".name").hide();
+ 
+      $("#search").keyup(function () {
+  
+          $(".name").hide();
+  
+          var val = $(this).val().toLowerCase();
+  
+          $(".name").each(function () {
+  
+              var text = $(this).text().toLowerCase();
+  
+  
+              if(val.length !== 0) { 
+  
+                  if (text.indexOf(val) !== -1 && val.charAt(val.length - 1) === text.charAt(val.length - 1)) {
+  
+                      $(this).show();
+              }  }
+              else{
+              $(".name").hide();
+              }
+          });
+      });
+  });
