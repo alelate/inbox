b2ca5f6cef3645915b2c6dce8a2e6daa7798fc20
Aleksejs <alelate69@gmail.com>
2015-06-04 01:57:12 +0300

    Updates script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-$(document).ready(function(){
-$(".name").hide();
-$("#search").keyup(function(){
-$(".name").hide();
-var val = $(this).val().toLowerCase();
-$(".name").hide();
-$(".name").each(function(){
-var text = $(this).text().toLowerCase();
+$(document).ready(function () {
 
-if(val.length < 2){
-if(text.charAt(0).indexOf(val.charAt(0)) !=-1 && val != 0){
-$(this).show();
-}
-}
-else{
-if(text.indexOf(val) !=-1 && val !==0){
-$(this).show();
-}
-}
+    $(".name").hide();
 
-});
-});
+    $("#search").keyup(function () {
+
+        $(".name").hide();
+
+        var val = $(this).val().toLowerCase();
+
+        $(".name").each(function () {
+
+            var text = $(this).text().toLowerCase();
+
+
+            if(val.length !== 0) { 
+
+                if (text.indexOf(val) !== -1 && val.charAt(val.length - 1) === text.charAt(val.length - 1)) {
+
+                    $(this).show();
+            }  }
+        });
+    });
 });
